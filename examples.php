a005a82bee916b82f6b9b4b52230e648d8abc9f9
Asya Nikitina <asya_nikitina@hotmail.com>
2018-06-28 18:01:56 -0600

    added final index page

---
+++ examples.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Examples of using PHP</title>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" href="./styles/style.css">
+	</head>
+	<body>
+		<div class="wrapper">
+		<p>MySQL is a robust and exceptionally fast database management system that uses English-like commands.<br>The
+			highest level of MySQL structure is a database, within which you can have one or more tables that contain
+			your data.<br>
+		Here is an example of <code>INSERT</code> command:<br><br>
+		<code>INSERT INTO users VALUES('Smith', 'John', 'jsmith@mysite.com');</code>
+			<br><br>This <code>INSERT</code> command is an example of <span class="emphasis">S</span>tructured
+			<span class="emphasis">Q</span>uery <span class="emphasis">L</span>anguage (<span class="emphasis">SQL</span>).
+		</p>
+			<p></p>
+		</div>
+	</body>
+</html>
--- index.php
+++ index.php
@@ -2,9 +2,17 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8" />
-		<title>I Completed the Prework</title>
+		<title>Prework</title>
+		<link rel="stylesheet" href="./styles/style.css">
 	</head>
 	<body>
+		<div class="wrapper">
+		<p>Today is <?php echo date("l"); ?>. Here is the latest news:
+		</p>
 		<p><strong>I completed the prework!</strong></p>
+		<br>
+		<p>Here are some illustrations of how PHP helps create dynamic web content:</p>
+		<a href="examples.php"><strong>Examples</strong></a>
+		</div>
 	</body>
 </html>
